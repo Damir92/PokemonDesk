d74e5910e82d41b57b22eb8fd09d731931b8becb
Damir92 <home_damir92@mail.ru>
2020-11-08 17:55:37 +0300

    fix

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import cn from 'classnames';
 
 import s from './Button.module.scss';
 
 interface ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  small?: boolean;
+  wide?: boolean;
+  yellow?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, small = false, wide = false, yellow = false }) => {
   return (
-    <button type="button" className={s.root} onClick={onClick}>
+    <button
+      type="button"
+      className={cn(s.root, { [s.yellow]: yellow }, { [s.wide]: wide }, { [s.small]: small })}
+      onClick={onClick}>
       {children}
     </button>
   );
--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -7,28 +7,9 @@ interface LayoutProps {
 }
 
 const Heading: React.FC<LayoutProps> = ({ children, level = 1 }) => {
-  switch (level) {
-    case 1:
-      return <h1 className={s.h1}>{children}</h1>;
+  const Tag: string = `h${level}`;
 
-    case 2:
-      return <h2 className={s.h2}>{children}</h2>;
-
-    case 3:
-      return <h3>{children}</h3>;
-
-    case 4:
-      return <h4>{children}</h4>;
-
-    case 5:
-      return <h5>{children}</h5>;
-
-    case 6:
-      return <h6>{children}</h6>;
-
-    default:
-      return <p>{children}</p>;
-  }
+  return <Tag className={s[Tag]}>{children}</Tag>;
 };
 
 export default Heading;
